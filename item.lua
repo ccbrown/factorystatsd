968d9a23da2de2139b6843374ff8d8e1312532f3
Christopher Brown <ccbrown112@gmail.com>
2021-09-18 21:01:22 -0400

    add statsd combinator item

---
+++ item.lua
@@ -0,0 +1,12 @@
+data:extend({
+    {
+        type = "item",
+        name = "statsd-combinator",
+        icon = "__factorystatsd__/graphics/icons/statsd-combinator.png",
+        icon_size = 64,
+        order = "c[combinators]-z[statsd-combinator]",
+        stack_size = 50,
+        subgroup = "circuit-network",
+        type = "item",
+    },
+})
